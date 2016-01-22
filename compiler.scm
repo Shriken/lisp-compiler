8ff06d9e8a3bc44f4ee7d280cd27c82488783b38
Zane Sterling <sterling.zane@gmail.com>
2016-01-21 22:42:09 -0500

    Add unary primitive handling

--- compiler.scm
+++ compiler.scm
@@ -3,7 +3,7 @@
 (define CHAR_TAG #xf)
 
 (define (compile-program x)
-	; converts immediate to binary format
+	; converts an immediate to binary format
 	(define (immediate-rep x)
 		(cond
 			((integer? x) (arithmetic-shift x FIXNUM_SHIFT))
@@ -13,12 +13,7 @@
 					(arithmetic-shift (char->integer x) CHAR_SHIFT)
 				)
 			)
-			((list? x)
-				(if (atom? x)
-					EMPTY_LIST
-					1 ; TODO implement non-empty list
-				)
-			)
+			((null? x) EMPTY_LIST)
 			((boolean? x)
 				(if x
 					#b10011111
@@ -28,6 +23,41 @@
 		)
 	)
 
+	(define (emit-expr x)
+		(cond
+			((immediate? x)
+				(print "	movl $" (immediate-rep x) ", %eax")
+			)
+			((primcall? x)
+				(case (primcall-op x)
+					((add1)
+						(emit-expr (primcall-operand1 x))
+						(print "	addl $" (immediate-rep 1) ", %eax")
+					)
+				)
+			)
+		)
+	)
+
+	(define (immediate? x)
+		(or
+			(integer? x)
+			(char? x)
+			(null? x)
+			(boolean? x)
+		)
+	)
+
+	(define (primcall? x)
+		(and
+			(not (atom? x))
+			(map atom? x)
+		)
+	)
+
+	(define primcall-op car)
+	(define primcall-operand1 cadr)
+
 	; function header
 	(print "	.text")
 	(print "	.p2align 4")
@@ -35,7 +65,7 @@
 	(print "_scheme_entry:")
 
 	; function body
-	(print "	movl $" (immediate-rep x) ", %eax")
+	(emit-expr `(add1 5))
 	(print "	ret")
 )
 
